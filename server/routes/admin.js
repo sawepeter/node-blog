b69c9a9d6132907378983dbd8e0dcdd8c70031f0
sawepeter <sawepeter6@gmail.com>
2023-08-18 16:15:02 +0300

    admin dash wip

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,7 +6,29 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
+
+/**
+ * 
+ * Check Login middleware
+ */
+const authMiddleware = (req, res, next ) => {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json( { message: 'Unauthorized'} );
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+        
+    } catch (error) {
+        res.status(401).json( { message: 'Unauthorized'} );
+    }
+}
 /** get admin login page */
 router.get('/admin', async (req, res) => {
 
@@ -22,17 +44,45 @@ router.get('/admin', async (req, res) => {
         console.log(error);
     }
 });
-/**Admin Check Login */
+/**
+ * POST 
+ * Admin Check Login 
+ */
 router.post('/admin', async (req, res) => {
     try {
 
         const { username, password } = req.body;
-        console.log(req.body);
-        res.redirect('/admin');
-        
+
+        const user = await User.findOne( { username } );
+
+        if (!user) {
+            return res.status(401).json( { message: 'Invalid credentials '} );
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json( { message: 'Invalid credentials '} );
+        }
+
+        const token = jwt.sign({ userId: user._id}, jwtSecret );
+        res.cookie('token', token, { httpOnly: true });
+
+        res.redirect('/dashboard');
+
     } catch (error) {
         console.log(error)
     }
+});
+
+/** 
+ * GET /
+ * Admin Dashboard
+ */
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    res.render('admin/dashboard');
+
 });
 
 /**Admin - Register */
