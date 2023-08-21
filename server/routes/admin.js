efadc245b55fbe21e37f70381d1c56cd35ca6fda
sawepeter <sawepeter6@gmail.com>
2023-08-21 17:19:10 +0300

    admin wip

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -104,6 +104,7 @@ router.get('/dashboard', authMiddleware, async (req, res) => {
 });
 
 /**
+ * GET
  * Admin - Create New Post
  */
 router.get('/add-post', authMiddleware, async (req, res) => {
@@ -123,6 +124,77 @@ router.get('/add-post', authMiddleware, async (req, res) => {
         console.log(error);
     }
 
+});
+
+/**
+ * POST
+ * Admin - Create New Post
+ */
+router.post('/add-post', authMiddleware, async (req, res) => {
+    try {
+        try {
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.body
+            });
+
+            await Post.create(newPost);
+            res.redirect('/dashboard');
+            
+        } catch (error) {
+            console.log(error);
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+/**
+ * GET
+ * Admin - single post
+ */
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+
+        const locals = {
+            title: "Edit Post",
+            description: "Free NodeJs User Management System",
+        };
+
+        const data = await Post.findOne({_id: req.params.id });
+
+        res.render('admin/edit-post', {
+            locals,
+            data,
+            layout: adminLayout
+        })
+        
+    } catch (error) {
+        console.log(error); 
+    }
+});
+
+/**
+ * PUT
+ * Admin - update single post
+ */
+router.put('edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+
+        await Post.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            body: req.body.body,
+            updatedAt: Date.now()
+        });
+
+        res.redirect(`/edit-post/$req.params.id`);
+
+        
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 /**Admin - Register */
@@ -149,4 +221,28 @@ router.post('/register', async (req, res) => {
         console.log(error)
     }
 });
+/**
+ * DELETE
+ * Admin - Delete Post
+ */
+router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id });
+        res.redirect('/dashboard');
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * GET
+ * Admin logout
+ */
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+
+    res.redirect('/');
+});
+
 module.exports = router;
